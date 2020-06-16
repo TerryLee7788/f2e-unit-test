706e8db6db6ee2af2d5c54dfe76dfc9da25d0c87
TerryLee <Terry.lee@ehsn.com.tw>
2020-06-16 15:47:14 +0800

    Resolve "Input:-42, target=-42" task case.

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -2,7 +2,20 @@
  * @param {string} str
  * @return {number}
  */
-const myAtoi = function(str) {
-    return 42
-};
+
+const myAtoi = function (str) {
+    var num = 0
+
+    // 去符號 + 空白
+    var sign = Math.sign(str.replace(/ +/i, ''))
+    // 去符號 + 空白 + 反轉字串
+    var newStr = str.replace(/[ -]+/i, '').split('').reverse().join('')
+
+    for (var i = 0; i < newStr.length; i++) {
+        num += newStr[i] * Math.pow(10, i)
+    }
+
+    return num * sign
+}
+
 export default myAtoi;
